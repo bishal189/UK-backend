f1e8917f9a80586e352705166583bb7c33b41483
bishal89 <bishalmurmu150@gmail.com>
2024-05-03 11:34:06 +0545

    added tempaltes

--- cosmetic/settings.py
+++ cosmetic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2hdtnk#&aez-(8&$z+6x)9&!^j8t#^ol%)d44fabx7e9f^)8hw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'cosmetic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,9 +115,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# for static files 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    context={
+        "name":12
+    }
+    return render(request,'index.html',context)
