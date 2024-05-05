1e73c9f9e22bedafcfcc776b3fa3556fd504028d
sandeshsitaula <navaltajohan@gmail.com>
2024-05-05 12:42:38 +0545

    updated to handle slug and removed product model

--- store/models.py
+++ store/models.py
@@ -3,52 +3,44 @@ from django.db import models
 from  django.urls import reverse
 from account.models import Account
 # Create your models here.
+from django.utils.text import slugify
 
 class Product(models.Model):
     product_name=models.CharField(max_length=500)
     slug=models.SlugField(max_length=200,unique=True)
     description=models.TextField(blank=True)
-    images=models.ImageField(upload_to='photos/products')
-    stock=models.IntegerField()
-    price=models.IntegerField()
+    image=models.ImageField(upload_to='products/small')
+    large_image=models.ImageField(upload_to='products/large')
+    stock=models.IntegerField(default=10)
+    price=models.DecimalField(max_digits=10,decimal_places=2)
     is_available=models.BooleanField(default=True)
     # category=models.ForeignKey(Category,on_delete=models.CASCADE)
     created_date=models.DateTimeField(auto_now_add=True)
     modified_at=models.DateTimeField(auto_now=True)
     created_by=models.ForeignKey(Account,on_delete=models.CASCADE,blank=True,null=True)
 
+    def save(self, *args, **kwargs):
+        # Generate slug from name
+        self.slug = slugify(self.product_name)
+
+        # Check if the slug already exists
+        if Product.objects.filter(slug=self.slug).exists():
+            # If slug already exists, add a counter to make it unique
+            counter = 1
+            while True:
+                new_slug = f"{self.slug}-{counter}"
+                if not Product.objects.filter(slug=new_slug).exists():
+                    self.slug = new_slug
+                    break
+                counter += 1
+
+        super().save(*args, **kwargs)
+
     def get_url(self):
         return reverse('product_details',args=[self.category.slug, self.slug])
     
     def __str__(self):
         return self.product_name
-
-
-# variation_category_choice = (
-#     ('color','color'),
-#     ('size','size'),
-# )
-
-
-# class Variationmanager(models.Manager):
-#     def colors(self):
-#         return super(Variationmanager,self).filter(variation_category='color',is_active=True)
-#     def sizes(self):
-#         return super(Variationmanager,self).filter(variation_category='size',is_active=True)    
-        
-              
-# class Variation(models.Model):
-#     product=models.ForeignKey(Product,on_delete=models.CASCADE)
-#     variation_category=models.CharField(max_length=100,choices=variation_category_choice) 
-#     variation_value=models.CharField(max_length=100,blank=True,null=True)
-#     is_active=models.BooleanField(default=True)
-#     created_date=models.DateTimeField(auto_now=True)
-#     objects=Variationmanager()
-
-
-
-    # def __str__(self):
-    #     return str(self.variation_value)
 
 
 class ReviewRating(models.Model):
