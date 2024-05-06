ee8e581bff0c3c49fd3d85f2aa53b493b2a17adf
sandeshsitaula <navaltajohan@gmail.com>
2024-05-06 16:55:19 +0545

    updated collection url in index page only for items for now

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ urlpatterns=[
     path('products/',views.products),
     path('products/run_script/',views.run_script),
     path('products/<str:product_name>/',views.get_product,name="get_product"),
-    path('collection/',views.collection,name="collection"),
+    path('collection/',views.collection,name="collections"),
     path('collection/<slug:collection_slug>/',views.collection,name="collection")
 
     ]
