b3b514c794e2406fd2cda25f4ac03a6d932fc4ca
sandeshsitaula <navaltajohan@gmail.com>
2024-05-06 17:07:24 +0545

    added views in home app to render footer links

--- home/urls.py
+++ home/urls.py
@@ -4,11 +4,15 @@ from . import views
 
 urlpatterns = [
  
-    path('',views.home,name='home'),
-    # path('collection/',views.collection,name='collection'),
-    # path('collections/',views.collections),
+    path('',views.index),
     path('cart/',views.cart),
-    path('product/',views.product),
+    path('about_us/',views.about_us,name="about_us"),
+    path('privacy_policy/',views.privacy_policy,name="privacy_policy"),
+    path('terms_and_conditions/',views.terms_and_conditions,name="terms_and_conditions"),
+    path('faqs/',views.faqs,name="faqs"),
+    path('contact_us/',views.contact_us,name="contact_us"),
+    path('delivery_and_returns/',views.delivery_and_returns,name="delivery_and_returns"),
+    path('customer_reviews/',views.customer_reviews,name="customer_reviews"),
 
 ]
 
