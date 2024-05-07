6d2194833a42e8994e56567da81fe493628a13e8
bishal89 <bishalmurmu150@gmail.com>
2024-05-07 15:34:53 +0545

    added the search functions

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from store.models import Product
+from django.db.models import Q
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -20,3 +21,21 @@ def delivery_and_returns(request):
     return render(request,'pages/delivery-and-returns.html')
 def customer_reviews(request):
     return render(request,'pages/customer-reviews.html')
+
+
+
+def search(request):
+    if 'keyword'  in request.GET:
+        keyword=request.GET['keyword']
+        print('i am get method')
+        if keyword:
+            products=Product.objects.order_by("-created_date").filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            count=products.count()
+
+        context={
+            'products':products,
+            'count':count
+        }
+
+    return render(request,'searchpage.html',context)
+
