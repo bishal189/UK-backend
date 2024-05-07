0c6a2555f59b5dcba71d0bc9ed82e38683df1581
sandeshsitaula <navaltajohan@gmail.com>
2024-05-07 16:57:49 +0545

    review model

--- review/models.py
+++ review/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from store.models import Product
 # Create your models here.
+
+class Review(models.Model):
+    name=models.TextField()
+    email=models.TextField()
+    review_title=models.TextField(blank=True,null=True)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    review_content=models.TextField()
+    created_at=models.DateField(auto_now_add=True)
+
+
+
