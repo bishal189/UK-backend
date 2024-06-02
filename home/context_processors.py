260aef793fe640ef5f893c7ec5a830cd9c5c80d9
sandeshsitaula <navaltajohan@gmail.com>
2024-06-02 14:20:18 +0545

    have https in cannonical link

--- home/context_processors.py
+++ home/context_processors.py
@@ -4,6 +4,9 @@ def collections_list_header(request):
     return {'collections_list_header': Collection.objects.all()}  # Assuming Collection is your model
 
 def canonical_url(request):
+    # Build the absolute URI and replace http with https
+    canonical_url = request.build_absolute_uri(request.path)
+    secure_canonical_url = canonical_url.replace('http://', 'https://')
     return {
-        'canonical_url': request.build_absolute_uri(request.path)
+        'canonical_url': secure_canonical_url
     }
